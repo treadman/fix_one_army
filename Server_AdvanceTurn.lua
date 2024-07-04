1a53294369a6ee01ae65cc7ecdc8d8963e1d4396
Tracy Treadway <treadmen@hotmail.com>
2024-07-04 15:59:19 -0400

    added more conditions

--- Server_AdvanceTurn.lua
+++ Server_AdvanceTurn.lua
@@ -1,14 +1,18 @@
 function Server_AdvanceTurn_Order(game, order, result, skipThisOrder, addNewOrder)
-	if(order.proxyType == 'GameOrderAttackTransfer') then
-        if(result.IsAttack) then
-            if(result.IsSuccessful == false) then
-                if(result.DefendingArmiesKilled.NumArmies == 0) then
-                    local terr = game.ServerGame.LatestTurnStanding.Territories[order.To];
-                    terrMod = WL.TerritoryModification.Create(terr.ID);
-                    terrMod.SetArmiesTo = 0;
-                    addNewOrder(WL.GameOrderEvent.Create(terr.OwnerPlayerID,"Defender Killed",{},{terrMod}));
+    if(game.ServerGame.Settings.OneArmyStandsGuard) then
+        if(game.ServerGame.Settings.OffenseKillRate >= 0.5) then
+            if(order.proxyType == 'GameOrderAttackTransfer') then
+                if(result.IsAttack) then
+                    if(result.IsSuccessful == false) then
+                        if(result.DefendingArmiesKilled.NumArmies == 0) then
+                            local terr = game.ServerGame.LatestTurnStanding.Territories[order.To];
+                            terrMod = WL.TerritoryModification.Create(terr.ID);
+                            terrMod.SetArmiesTo = 0;
+                            addNewOrder(WL.GameOrderEvent.Create(terr.OwnerPlayerID,"Defender Killed",{},{terrMod}));
+                        end
+                    end
                 end
             end
         end
-	end
+    end
 end
